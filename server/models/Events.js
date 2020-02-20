3493c86ebd003b3bc7d7a42c405f3f503c01c0ea
gersonbarajs62 <gersonbarajas62@gmail.com>
2020-02-19 19:42:08 -0600

    changes done to Events

--- server/models/Events.js
+++ server/models/Events.js
@@ -11,5 +11,42 @@ const EventsSchema = new Schema({
 
     description:{
         type:String,
-    }
+        remoquire:true
+    },
+
+    date:{
+        type:Date
+    },
+    
+    address:{
+        street:String,
+        city:String,
+        number:String,
+        country:String,
+        state:String,
+        zip:String,
+        lat:Number,
+        long:Number
+
+    },
+
+    assistants:{
+        type:[Schema.Types.ObjectId],
+        ref:'users'
+    },
+
+    created_by:{
+        type:Schema.Types.ObjectId,
+        ref:'users'
+    },
+
+    banner:{
+        type:String
+    },
+    
+    tags:{
+        type:[]
+    },
+
+    
 })
