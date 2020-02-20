bfbf974fd4d9de895853e2345ca248e9acd851a6
gersonbarajs62 <gersonbarajas62@gmail.com>
2020-02-19 20:35:40 -0600

    changes done index.js and added dependencies

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose'),
+
+/*const MONGO_URI = "mongodb+srv://gersonroa:<password>@hiring-portal-huipr.mongodb.net/test?retryWrites=true&w=majority" 
+
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+const mongo = mogoose.connection;
+mongo.on('error', error => console.log(error) )
+    .once('open', () => console.log('connected to database'))*/
+
 
 
 const typeDefs = gql`
